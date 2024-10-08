05d7ba64cd7292811529e4209818263fec56f849
elizastudiomade <67680907+elizastudiomade@users.noreply.github.com>
2024-10-08 10:30:34 +0100

    initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'book-track-app',
+  webDir: 'dist'
+};
+
+export default config;
